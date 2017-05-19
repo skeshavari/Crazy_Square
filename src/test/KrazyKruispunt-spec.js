f00e7354a046910c5db558f70151c0126621fff7
CKYong <ckyoung@sogyo.nl>
2017-05-19 13:46:53 +0200

    Deleted old java files, included Karma and Jasmine for unit testing.

---
+++ src/test/KrazyKruispunt-spec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ckyoung on 19-May-17.
+ */
+(function () {
+    describe("the game", function () {
+        it("should exist", function(){
+            expect('KrazyKruispunt.js').toBeDefined();
+        })
+    })
+})();
