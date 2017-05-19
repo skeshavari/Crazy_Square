da636ee23fa4ea7a2f579e34a884ab580994bc5e
Peter Hijma <phijma@sogyo.nl>
2017-05-19 17:09:26 +0200

    auto smooth movement

--- phaser-kruispunt/math.js
+++ phaser-kruispunt/math.js
@@ -1,7 +1,20 @@
 function lerp(a, b, amount) {
-	if (Math.abs(a - b) < 0.01) {
-		return a;
-	}
+	if (Math.abs(a - b) < 0.1) {
+		return b;
+	} 
 
 	return a + (b - a)*amount;
 }
+
+
+function lerp_angle(a, b, amount) {
+	if (Math.abs(a - b) < 0.1) {
+		return b;
+	} else if (b < a) {
+		return a + (b - a)*amount;
+	} else {
+		return a + (b - a)*amount;
+	}
+	
+}
+
