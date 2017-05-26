83732b5f30cfe780ecde4275d6148c96145424f9
Sven Schipper <sschipper@sogyo.nl>
2017-05-26 16:51:06 +0200

    more control over TrafficLights

--- phaser-kruispunt/trafficlight.js
+++ phaser-kruispunt/trafficlight.js
@@ -1,9 +1,9 @@
-function TrafficLight(colour) {
+function TrafficLight(colour, locX, locY) {
 var red = 0xFF0000, yellow = 0xFFFF00, green = 0x00FF00;
 var colour;
 
     trafficLightGraphics = game.add.graphics(0, 0);
     trafficLightGraphics.beginFill(colour)
-    trafficLightGraphics.drawRect(225, 125, 50, 50);
+    trafficLightGraphics.drawRect(locX, locY, 50, 50);
     trafficLightGraphics.endFill();
     }
