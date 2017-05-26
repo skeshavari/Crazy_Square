e6a00189c53dee3be87503cba8d321cbab8fb882
svheeringen <svanheeringen@sogyo.nl>
2017-05-26 17:08:44 +0200

    Game and Test

--- src/main/KrazyKruispunt.js
+++ src/main/KrazyKruispunt.js
@@ -1,3 +1,60 @@
 /**
  * Created by ckyoung on 19-May-17.
  */
+Game = (function() {
+    var cars = [];
+    var lights = [];
+
+    return {
+        clearTest: function() {
+            lights = [];
+            cars = [];
+        },
+        makeCar: function(x,y,facing) {
+            cars.push({
+                locX: x,
+                locY: y,
+                direction: facing,
+                moveForward: function() {
+                    switch (facing) {
+                        case "north":
+                            this.locX += 1;
+                            break;
+                        case "south":
+                            this.locX -= 1;
+                            break;
+                        case "east":
+                            this.locY += 1;
+                            break;
+                        case "west":
+                            this.locY -= 1;
+                            break;
+                    }
+                }
+            });
+        },
+        getCars: function() {
+            return cars;
+        },
+        makeTrafficLight: function(x,y) {
+            lights.push({
+                locX: x,
+                locY: y,
+                color: "red",
+                changeColor: function() {
+                    if (this.color === "red") {
+                        this.color = "green";
+                    } else if (this.color === "green") {
+                        this.color = "red";
+                    }
+                },
+                getColor: function() {
+                    return this.color;
+                }
+            });
+        },
+        getTrafficLights: function() {
+            return lights;
+        }
+    };
+})();
--- src/test/KrazyKruispunt-spec.js
+++ src/test/KrazyKruispunt-spec.js
@@ -2,9 +2,44 @@
  * Created by ckyoung on 19-May-17.
  */
 (function () {
-    describe("the game", function () {
+    describe("The game", function () {
         it("should exist", function(){
-            expect('KrazyKruispunt.js').toBeDefined();
+            expect(Game).toBeDefined();
         })
-    })
+    });
+    describe("The cars", function() {
+        it("should have been added", function() {
+            Game.makeCar(3, 5, "south");
+            Game.makeCar(1, 3, "east");
+            Game.makeCar(5, 4, "west");
+            expect(Game.getCars().length).toBe(3);
+            Game.clearTest();
+        });
+    });
+    describe("The traffic lights", function() {
+        it("should have been added", function() {
+            Game.makeTrafficLight(4, 2);
+            Game.makeTrafficLight(5, 4);
+            Game.makeTrafficLight(3, 5);
+            Game.makeTrafficLight(2, 3);
+            expect(Game.getTrafficLights().length).toBe(4);
+            Game.clearTest();
+        });
+    });
+    describe("A traffic light", function() {
+        it("can change color to green", function() {
+            Game.makeTrafficLight(4, 2);
+            Game.getTrafficLights()[0].changeColor();
+            expect(Game.getTrafficLights()[0].getColor()).toBe("green");
+            Game.clearTest();
+        });
+        it("can change color to red, if it isn't already", function() {
+            Game.makeTrafficLight(4, 2);
+            Game.getTrafficLights()[0].color = "green";
+            Game.getTrafficLights()[0].changeColor();
+            expect(Game.getTrafficLights()[0].getColor()).toBe("red");
+            Game.clearTest();
+        });
+    });
+
 })();
