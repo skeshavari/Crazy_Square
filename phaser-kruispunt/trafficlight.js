a49aab769453caa7f1678cdcb61fcf395e6e9772
Sven Schipper <sschipper@sogyo.nl>
2017-05-26 16:48:29 +0200

    cleaned up trafficLights

--- phaser-kruispunt/trafficlight.js
+++ phaser-kruispunt/trafficlight.js
@@ -1,44 +1,9 @@
-function TrafficLight(game, colour) {
+function TrafficLight(colour) {
 var red = 0xFF0000, yellow = 0xFFFF00, green = 0x00FF00;
-var colour, trafficLightGraphics = new Object();
+var colour;
 
     trafficLightGraphics = game.add.graphics(0, 0);
     trafficLightGraphics.beginFill(colour)
     trafficLightGraphics.drawRect(225, 125, 50, 50);
     trafficLightGraphics.endFill();
     }
-
-    // updateColour = function(colour) {
-    //     trafficLightGraphics.destroy();
-    //     trafficLightGraphics = game.add.graphics(0, 0);
-    //     trafficLightGraphics.beginFill(colour)
-    //     trafficLightGraphics.drawRect(225, 125, 50, 50);
-    //     trafficLightGraphics.endFill();
-    //     return trafficLightGraphics;
-    // }
-//   }
-// })
-
-/*green = function() {
-    //trafficLightGraphics.destroy();
-    trafficLightGraphics = game.add.graphics(0, 0);
-    trafficLightGraphics.beginFill(0x00FF00)
-    trafficLightGraphics.drawRect(225, 125, 50, 50);
-    trafficLightGraphics.endFill();
-}
-
-yellow = function() {
-    trafficLightGraphics.destroy();
-    trafficLightGraphics = game.add.graphics(0, 0);
-    trafficLightGraphics.beginFill(0xFFFF00)
-    trafficLightGraphics.drawRect(225, 125, 50, 50);
-    trafficLightGraphics.endFill();
-}
-
-red = function() {
-    trafficLightGraphics.destroy();
-    trafficLightGraphics = game.add.graphics(0, 0);
-    trafficLightGraphics.beginFill(0xFF0000)
-    trafficLightGraphics.drawRect(225, 125, 50, 50);
-    trafficLightGraphics.endFill();
-}*/
