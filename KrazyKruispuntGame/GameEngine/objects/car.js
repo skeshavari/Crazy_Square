a191f081bf21c71334868afef4727c4a50ae18c5
Peter Hijma <phijma@sogyo.nl>
2017-06-29 16:16:44 +0200

    working knipperlichten

--- KrazyKruispuntGame/GameEngine/objects/car.js
+++ KrazyKruispuntGame/GameEngine/objects/car.js
@@ -41,31 +41,25 @@ Car = (function () {
                     break;
             }
 
-            switch (route) {
-                case "forward":
-                    knipper_x = sprite.x - 25;
-                    knipper_y = sprite.x - 50;
-                    break;
-                case "left":
-                    knipper_x = sprite.x - 25;
-                    knipper_y = sprite.x - 50;
-                    break;
-                case "right":
-                    knipper_x = sprite.x + 25;
-                    knipper_y = sprite.x - 50;
-                    break;
-            }
+            var knipper_x = sprite.x + 25;
+            var knipper_y = sprite.x + 25;
 
+
+            if (route === "forward") {
+                knipperlicht = undefined;
+            } else {
+                var knipperlicht = game.add.sprite(knipper_x, knipper_y, 'knipperendlicht');
+                knipperlicht.animations.add('knipper');
+                knipperlicht.animations.play('knipper', 1, true);
+                knipperlicht.animations.currentAnim.speed = 2;
+            }
+                    
             var emitter_smoke = game.add.emitter(game.world.centerX, game.world.centerY, 400);
             emitter_smoke.makeParticles(['p_smoke']);
             emitter_smoke.setAlpha(0.2, 1, 1000);
             emitter_smoke.setScale(0.25, 0, 0.25, 0, 250);
             emitter_smoke.start(false, 1000, 50);
 
-            var knipperlicht = game.add.sprite(knipper_x, knipper_y, 'knipperendlicht');
-            knipperlicht.animations.add('knipper');
-            knipperlicht.animations.play('knipper', 1, true);
-            knipperlicht.animations.currentAnim.speed = 2;
 
             carSprites.push({
                 sprite: sprite,
@@ -114,24 +108,59 @@ Car = (function () {
                 carSprites[i].emitter_smoke.emitX = particle_x;
                 carSprites[i].emitter_smoke.emitY = particle_y;
 
-                for (var c = 0; c < carSprites.length; c++) {
-                    switch (carSprites[c].route) {
-                        case "forward":
-                            carSprites[c].knipper_x = carSprites[c].sprite.x - 22;
-                            carSprites[c].knipper_y = carSprites[c].sprite.y - 36;
-                            break;
-                        case "left":
-                            carSprites[c].knipper_x = carSprites[c].sprite.x - 22;
-                            carSprites[c].knipper_y = carSprites[c].sprite.y - 36;
-                            break;
-                        case "right":
-                            carSprites[c].knipper_x = carSprites[c].sprite.x + 22;
-                            carSprites[c].knipper_y = carSprites[c].sprite.y - 36;
-                            break;
-                    }
+                // KNIPPERLICHTEN GEDOE:
+                switch (carSprites[i].route) {
 
-                    carSprites[c].knipperlicht.x = carSprites[c].knipper_x;
-                    carSprites[c].knipperlicht.y = carSprites[c].knipper_y;
+                    case "left":
+
+                        switch (carSprites[i].orientation) {
+                            case "south":
+                                carSprites[i].knipper_x = carSprites[i].sprite.x + 10;
+                                carSprites[i].knipper_y = carSprites[i].sprite.y + 25;
+                                break;
+                            case "north":
+                                carSprites[i].knipper_x = carSprites[i].sprite.x - 20;
+                                carSprites[i].knipper_y = carSprites[i].sprite.y - 32;
+                                break;
+                            case "east":
+                                carSprites[i].knipper_x = carSprites[i].sprite.x + 23;
+                                carSprites[i].knipper_y = carSprites[i].sprite.y - 20;
+                                break;
+                            case "west":
+                                carSprites[i].knipper_x = carSprites[i].sprite.x - 30;
+                                carSprites[i].knipper_y = carSprites[i].sprite.y + 10;
+                                break;
+                        }
+
+                        break;
+
+                    case "right":
+
+                        switch (carSprites[i].orientation) {
+                            case "south":
+                                carSprites[i].knipper_x = carSprites[i].sprite.x - 10;
+                                carSprites[i].knipper_y = carSprites[i].sprite.y + 25;
+                                break;
+                            case "north":
+                                carSprites[i].knipper_x = carSprites[i].sprite.x + 10;
+                                carSprites[i].knipper_y = carSprites[i].sprite.y - 32;
+                                break;
+                            case "east":
+                                carSprites[i].knipper_x = carSprites[i].sprite.x + 20;
+                                carSprites[i].knipper_y = carSprites[i].sprite.y + 10;
+                                break;
+                            case "west":
+                                carSprites[i].knipper_x = carSprites[i].sprite.x - 30;
+                                carSprites[i].knipper_y = carSprites[i].sprite.y - 20;
+                                break;
+                        }
+
+                        break;
+                }
+
+                if (carSprites[i].knipperlicht !== undefined) {
+                    carSprites[i].knipperlicht.x = carSprites[i].knipper_x;
+                    carSprites[i].knipperlicht.y = carSprites[i].knipper_y;
                 }
 
                 carSprites[i].sprite.angle = carSprites[i].lerp_angle;
@@ -144,7 +173,9 @@ Car = (function () {
                     playExplosion(carSprites[index[i]].sprite.x, carSprites[index[i]].sprite.y);
                     carSprites[index[i]].sprite.destroy();
                     carSprites[index[i]].emitter_smoke.destroy();
-                    carSprites[index[i]].knipperlicht.destroy();
+                    if (carSprites[index[i]].knipperlicht !== undefined) {
+                        carSprites[index[i]].knipperlicht.destroy();
+                    }
                     carSprites.splice(index[i], 1);
                 }
             }
@@ -154,13 +185,20 @@ Car = (function () {
                 for (var j = 0; j < difference; j++) {
                     this.create(toGridX(carObjects[carSprites.length + j].getX()) + 50,
                         toGridY(carObjects[carSprites.length + j].getY()) + 50,
-                        carObjects[carSprites.length + j].getDirection());
+                        carObjects[carSprites.length + j].getDirection(),
+                        carObjects[carSprites.length + j].getRoute());
                 }
             }
             //Update car position
             for (var c = 0; c < carObjects.length; c++) {
                 carSprites[c].lerp_x = toGridX(carObjects[c].getX()) + 50;
                 carSprites[c].lerp_y = toGridY(carObjects[c].getY()) + 50;
+
+                // CHECK IF CAR HAS TURNED SO KNIPPERLICHT GOES OFF:
+                if (carSprites[c].orientation !== carObjects[c].getDirection() && carSprites[c].knipperlicht !== undefined) {
+                    carSprites[c].knipperlicht.destroy();
+                }
+
                 carSprites[c].orientation = carObjects[c].getDirection();
             }
         },
